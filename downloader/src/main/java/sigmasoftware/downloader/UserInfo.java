c435ec596f7947c25c741eb15f34bf70d4d2888e
Kateryna Oblakevych <kateryna.oblakevych@sigma.software>
2021-05-15 12:17:56 +0300

    DL-3: Add use information class.

---
+++ downloader/src/main/java/sigmasoftware/downloader/UserInfo.java
@@ -0,0 +1,60 @@
+package sigmasoftware.downloader;
+
+public class UserInfo {
+
+    private String name;
+    private String surname;
+    private String country;
+    private String city;
+    private String phoneNumber;
+    private String age;
+    private String nationality;
+
+    public static class Builder {
+        private UserInfo userInfo;
+
+        public Builder() {
+            userInfo = new UserInfo();
+        }
+
+        public UserInfo build(){
+            return userInfo;
+        }
+
+        public Builder name(String name) {
+            userInfo.name = name;
+            return this;
+        }
+
+        public Builder surname(String surname) {
+            userInfo.surname = surname;
+            return this;
+        }
+
+        public Builder country(String country) {
+            userInfo.country = country;
+            return this;
+        }
+
+        public Builder city(String city) {
+            userInfo.city = city;
+            return this;
+        }
+
+        public Builder phoneNumber(String phoneNumber) {
+            userInfo.phoneNumber = phoneNumber;
+            return this;
+        }
+
+        public Builder age(String age) {
+            userInfo.age = age;
+            return this;
+        }
+
+        public Builder nationality(String nationality) {
+            userInfo.nationality = nationality;
+            return this;
+        }
+    }
+
+}
