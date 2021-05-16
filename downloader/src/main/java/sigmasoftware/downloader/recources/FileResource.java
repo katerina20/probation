4148598e13477e1f945daeb7331f1027352ad085
Kateryna Oblakevych <kateryna.oblakevych@sigma.software>
2021-05-16 15:45:23 +0300

    DL-7: Add spring security with jdbc.

--- downloader/src/main/java/sigmasoftware/downloader/recources/FileResource.java
+++ downloader/src/main/java/sigmasoftware/downloader/recources/FileResource.java
@@ -1,23 +1,25 @@
-package sigmasoftware.downloader;
+package sigmasoftware.downloader.recources;
 
 import io.quarkus.qute.Template;
 import io.quarkus.qute.TemplateInstance;
 
+import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/info")
-public class UserInfoResource {
+@Path("/files")
+public class FileResource {
 
     @Inject
-    Template info;
+    Template files;
 
     @GET
+    @RolesAllowed("ADMIN")
     @Produces(MediaType.TEXT_HTML)
     public TemplateInstance get() {
-        return info.instance();
+        return files.instance();
     }
 }
