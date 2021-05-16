022824672babf57bd3fe4f32f9b6ca4daa321f8a
Kateryna Oblakevych <kateryna.oblakevych@sigma.software>
2021-05-16 12:57:15 +0300

    DL-5: Add html templates.

---
+++ downloader/src/main/java/sigmasoftware/downloader/UserInfoResource.java
@@ -0,0 +1,23 @@
+package sigmasoftware.downloader;
+
+import io.quarkus.qute.Template;
+import io.quarkus.qute.TemplateInstance;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/info")
+public class UserInfoResource {
+
+    @Inject
+    Template info;
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public TemplateInstance get() {
+        return info.instance();
+    }
+}
