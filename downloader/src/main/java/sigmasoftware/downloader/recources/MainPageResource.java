ba0aac4a33dae8128f6eedf64352a9d919402f15
Kateryna Oblakevych <kateryna.oblakevych@sigma.software>
2021-05-20 12:55:13 +0300

    DL-11: Add file downloading.

--- downloader/src/main/java/sigmasoftware/downloader/recources/MainPageResource.java
+++ downloader/src/main/java/sigmasoftware/downloader/recources/MainPageResource.java
@@ -2,10 +2,10 @@ package sigmasoftware.downloader.recources;
 
 import io.quarkus.qute.Template;
 import io.quarkus.qute.TemplateInstance;
-import sigmasoftware.downloader.dto.UserInfo;
 import sigmasoftware.downloader.services.UserService;
 
 import javax.annotation.security.PermitAll;
+import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -16,7 +16,7 @@ import javax.ws.rs.core.SecurityContext;
 import java.security.Principal;
 
 @Path("/main")
-@PermitAll
+@RolesAllowed({"ADMIN", "USER"})
 public class MainPageResource {
 
     @Inject
